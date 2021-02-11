6f793c8ca0372dc86b3327ccc08e9c83d8f3405d
strike9906 <ivan.klepinin00@gmail.com>
2021-02-11 20:39:15 +0200

    1

--- index.js
+++ index.js
@@ -20,14 +20,30 @@ app.use('/api/auth', require('./routes/auth.routes'))
 //         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 //     })
 // }
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-}
+  }
+
 const PORT = process.env.PORT || 4000
 let games = [];
 let nextGameId = 0;
